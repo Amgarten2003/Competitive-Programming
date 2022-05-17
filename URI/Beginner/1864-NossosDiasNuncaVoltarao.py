4327aad692e0158ded1b1a2e9c43668e4fbff6df
João Amgarten <78649250+Amgarten2003@users.noreply.github.com>
2022-05-17 14:17:41 +0100

    Create 1864-NossosDiasNuncaVoltarao.py

---
+++ URI/Beginner/1864-NossosDiasNuncaVoltarao.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+
+sentence = 'LIFE IS NOT A PROBLEM TO BE SOLVED'
+user_range = int(input())
+
+print(sentence[0:user_range])
